bee42ade57ec84ba18751837d8444ee3a629363e
Kanad Bhattacharya <kanadb004@gmail.com>
2026-06-02 17:55:16 +0530

    Added Carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+import classNames from "classnames";
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % imageUrls.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrevious = () => {
+    const previousIndex =
+      (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+    setCurrentIndex(previousIndex);
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="flex items-center gap-2">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={handleNext}
+        />
+      </div>
+      <div className="flex gap-2 space-x-1">
+        {/* {imageUrls.map((_, index) => {
+                    const defaultClasses =
+                        "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border";
+
+                    const dotClassNames =
+                        index === currentIndex
+                        ? defaultClasses.concat(" neeto-ui-bg-black")
+                        : defaultClasses;
+
+                    return (
+                        <span
+                            className={dotClassNames}
+                            key={index}
+                            onClick={() => setCurrentIndex(index)}
+                        />
+                    );
+                })} */}
+
+        {imageUrls.map((_, index) => (
+          <span
+            key={index}
+            className={classNames(
+              "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
+              { "neeto-ui-bg-black": index === currentIndex }
+            )}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
