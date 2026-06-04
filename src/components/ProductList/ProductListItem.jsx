bf43840e58018fd1a699145d8844163aa6e699fc
Kanad Bhattacharya <kanadb004@gmail.com>
2026-06-04 16:14:46 +0530

    Added feature to add products to cart

--- src/components/ProductList/ProductListItem.jsx
+++ src/components/ProductList/ProductListItem.jsx
@@ -1,9 +1,17 @@
+import AddToCart from "components/AddToCart";
 import { Typography } from "neetoui";
 import { Link } from "react-router-dom";
 import routes from "routes";
 import { buildUrl } from "utils/url";
 
-const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
+const ProductListItem = ({
+  imageUrl,
+  name,
+  offerPrice,
+  isInCart,
+  toggleIsInCart,
+  slug,
+}) => (
   <Link
     className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
     // to={`products/${slug}`}
@@ -14,6 +22,7 @@ const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
       {name}
     </Typography>
     <Typography>${offerPrice}</Typography>
+    <AddToCart {...{ isInCart, toggleIsInCart }} />
   </Link>
 );
 
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -5,13 +5,16 @@ import { Header, PageLoader } from "components/commons";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, without } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
+  const [cartItems, setCartItems] = useState([]);
+
   const [searchKey, setSearchKey] = useState("");
   const debouncedSearchKey = useDebounce(searchKey);
+
   const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
@@ -34,9 +37,17 @@ const ProductList = () => {
     return <PageLoader />;
   }
 
+  const toggleIsInCart = slug =>
+    setCartItems(prevCartItems =>
+      prevCartItems.includes(slug)
+        ? without([slug], cartItems)
+        : [slug, ...cartItems]
+    );
+
   return (
     <div className="flex h-screen flex-col">
       <Header
+        cartItemsCount={cartItems.length}
         shouldShowBackButton={false}
         title="Smile cart"
         actionBlock={
@@ -54,7 +65,12 @@ const ProductList = () => {
       ) : (
         <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
           {products.map(product => (
-            <ProductListItem key={product.slug} {...product} />
+            <ProductListItem
+              key={product.slug}
+              {...product}
+              isInCart={cartItems.includes(product.slug)}
+              toggleIsInCart={() => toggleIsInCart(product.slug)}
+            />
           ))}
         </div>
       )}
