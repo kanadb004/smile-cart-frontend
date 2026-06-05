8615e19247b9c7b01f982e5713919db3ad704279
Kanad Bhattacharya <kanadb004@gmail.com>
2026-06-06 01:50:06 +0530

    Adjusted ProductList component to use query parameter info

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,21 +2,36 @@ import { useState } from "react";
 
 import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
-import useDebounce from "hooks/useDebounce";
+import useFuncDebounce from "hooks/useFuncDebounce";
+import useQueryParams from "hooks/useQueryParams";
+import { filterNonNull } from "neetocist";
 import { Search } from "neetoicons";
 import { Input, NoData, Pagination } from "neetoui";
-import { isEmpty, without } from "ramda";
+import { isEmpty, without, mergeLeft } from "ramda";
+import { useHistory } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
 import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
-  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
+  const queryParams = useQueryParams();
+  const { page, pageSize, searchTerm = "" } = queryParams;
+
+  const [searchKey, setSearchKey] = useState(searchTerm);
+
+  // const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
+
+  // const queryParams = useQueryParams();
+  // const { page, pageSize } = queryParams
 
   const [cartItems, setCartItems] = useState([]);
 
-  const [searchKey, setSearchKey] = useState("");
-  const debouncedSearchKey = useDebounce(searchKey);
+  // const [searchKey, setSearchKey] = useState("");
+  // const debouncedSearchKey = useDebounce(searchKey);
+
+  const history = useHistory();
 
   // const [isLoading, setIsLoading] = useState(true);
   // const [products, setProducts] = useState([]);
@@ -26,13 +41,34 @@ const ProductList = () => {
   // });
 
   const productsParams = {
-    searchTerm: debouncedSearchKey,
-    page: currentPage,
-    pageSize: DEFAULT_PAGE_SIZE,
+    searchTerm,
+    // page: currentPage,
+    // pageSize: DEFAULT_PAGE_SIZE,
+    page: Number(page) || DEFAULT_PAGE_INDEX,
+    pageSize: Number(pageSize) || DEFAULT_PAGE_SIZE,
   };
+
+  const handlePageNavigation = page =>
+    history.replace(
+      buildUrl(
+        routes.products.index,
+        mergeLeft({ page, pageSize: DEFAULT_PAGE_SIZE }, queryParams)
+      )
+    );
 
   const { data: { products = [], totalProductsCount } = {}, isLoading } =
     useFetchProducts(productsParams);
+
+  // const updateQueryParams = ({ target: { value } }) => {
+  const updateQueryParams = useFuncDebounce(value => {
+    const params = {
+      page: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+      searchTerm: value || null,
+    };
+
+    history.replace(buildUrl(routes.products.index, filterNonNull(params)));
+  });
 
   // const fetchProducts = async () => {
   //   try {
@@ -73,9 +109,14 @@ const ProductList = () => {
             type="search"
             value={searchKey}
             // onChange={event => setSearchKey(event.target.value)}
-            onChange={e => {
-              setSearchKey(e.target.value);
-              setCurrentPage(DEFAULT_PAGE_INDEX);
+            // onChange={e => {
+            //   setSearchKey(e.target.value);
+            //   setCurrentPage(DEFAULT_PAGE_INDEX);
+            // }}
+            // onChange={updateQueryParams}
+            onChange={({ target: { value } }) => {
+              updateQueryParams(value);
+              setSearchKey(value);
             }}
           />
         }
@@ -97,9 +138,12 @@ const ProductList = () => {
       <div className="mb-5 self-end">
         <Pagination
           count={totalProductsCount}
-          navigate={page => setCurrentPage(page)}
-          pageNo={currentPage || DEFAULT_PAGE_INDEX}
-          pageSize={DEFAULT_PAGE_SIZE}
+          // navigate={page => setCurrentPage(page)}
+          navigate={handlePageNavigation}
+          // pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          // pageSize={DEFAULT_PAGE_SIZE}
+          pageNo={Number(page) || DEFAULT_PAGE_INDEX}
+          pageSize={Number(pageSize) || DEFAULT_PAGE_SIZE}
         />
       </div>
     </div>
