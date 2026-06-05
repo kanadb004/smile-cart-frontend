8ba75ef48e6886bf8bee01bc1e4b1af53df9b594
Kanad Bhattacharya <kanadb004@gmail.com>
2026-06-06 02:14:22 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
-
-import productsApi from "apis/products";
 import { PageLoader } from "components/commons";
 import Header from "components/commons/Header";
 import { MRP, OFFER_PRICE } from "components/constants";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
-import { NoData, Toastr } from "neetoui";
+import { NoData } from "neetoui";
 import { keys, isEmpty } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
 import withTitle from "utils/withTitle";
@@ -25,47 +23,47 @@ const Cart = () => {
   //     }),
   //     shallow
   // );
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
+  //   const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
 
-  const slugs = keys(cartItems);
+  //   const slugs = keys(cartItems);
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
 
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
+
+  //   const [products, setProducts] = useState([]);
+  //   const [isLoading, setIsLoading] = useState(true);
 
   const totalMrp = cartTotalOf(products, MRP);
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
 
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productsApi.show(slug))
-      );
+  //   const fetchCartProducts = async () => {
+  //     try {
+  //       const responses = await Promise.all(
+  //         slugs.map(slug => productsApi.show(slug))
+  //       );
 
-      setProducts(responses);
+  //       setProducts(responses);
 
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
+  //       responses.forEach(({ availableQuantity, name, slug }) => {
+  //         if (availableQuantity >= cartItems[slug]) return;
 
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          Toastr.error(
-            `${name} is no longer available and has been removed from cart`,
-            {
-              autoClose: 2000,
-            }
-          );
-        }
-      });
-    } catch (error) {
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  //         setSelectedQuantity(slug, availableQuantity);
+  //         if (availableQuantity === 0) {
+  //             Toastr.error(t("product.error.removedFromCart", { name }), {
+  //             autoClose: 2000,
+  //             });
+  //         }
+  //       });
+  //     } catch (error) {
+  //       console.log("An error occurred:", error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
 
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
+  //   useEffect(() => {
+  //     fetchCartProducts();
+  //   }, [cartItems]);
 
   if (isLoading) return <PageLoader />;
 
