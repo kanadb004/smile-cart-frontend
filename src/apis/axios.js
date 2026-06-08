9f0bc23aaef8573eab4e4d4124cf9d33eb4f3944
Kanad Bhattacharya <kanadb004@gmail.com>
2026-06-08 08:35:07 +0530

    Added submission handler to forms

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,10 +1,40 @@
 import axios from "axios";
-import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
-import { evolve } from "ramda";
+import { t } from "i18next";
+import { keysToCamelCase } from "neetocist";
+import { Toastr } from "neetoui";
+
+const shouldShowToastr = response =>
+  typeof response === "object" && response?.noticeCode;
+
+const showSuccessToastr = response => {
+  if (shouldShowToastr(response.data)) Toastr.success(response.data);
+};
+
+const showErrorToastr = error => {
+  if (error.message === t("error.networkError")) {
+    Toastr.error(t("error.noInternetConnection"));
+  } else if (error.response?.status !== 404) {
+    Toastr.error(error);
+  }
+};
 
 const requestInterceptors = () => {
-  axios.interceptors.request.use(
-    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  // axios.interceptors.request.use(
+  //   evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  // );
+
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+      showSuccessToastr(response);
+
+      return response.data;
+    },
+    error => {
+      showErrorToastr(error);
+
+      return Promise.reject(error);
+    }
   );
 };
 
